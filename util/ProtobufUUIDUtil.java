a3329f07024f7cc63fd8d8987816c876bae100fe
Adam Mitchell <adam@grakn.ai>
2020-02-21 16:43:51 +0000

    Added comments for the UUID utility

--- util/ProtobufUUIDUtil.java
+++ util/ProtobufUUIDUtil.java
@@ -5,6 +5,15 @@ import com.google.protobuf.ByteString;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
+
+/**
+ * A utility class for converting between {@link UUID} and a protobuf byte array containing the 4 most significant bytes
+ * followed by the 4 least significant bytes. This packs the UUID more efficiently than using a {@link String}.
+ *
+ * The ordering of the bytes follows Java's {@link ByteBuffer} convention. {@link ByteString}s made using this class
+ * will always correctly be deserialized regardless of system endianness since Java defines its own endianness and
+ * protobuf preserves it.
+ */
 public class ProtobufUUIDUtil {
     private enum EmptyUUID {
         INSTANCE;
@@ -12,6 +21,12 @@ public class ProtobufUUIDUtil {
         private UUID emptyUuid = new UUID(0, 0);
     }
 
+    /**
+     * Converts a protobuf {@link ByteString} into a Java {@link UUID}.
+     *
+     * @param uuid A protobuf UUID (MSB, LSB)
+     * @return The equivalent Java UUID.
+     */
     public static UUID fromBuf(ByteString uuid) {
         if (uuid == null || uuid.isEmpty()) {
             return EmptyUUID.INSTANCE.emptyUuid;
@@ -23,6 +38,12 @@ public class ProtobufUUIDUtil {
         return new UUID(msb, lsb);
     }
 
+    /**
+     * Converts a Java {@link UUID} into a protobuf {@link ByteString}.
+     *
+     * @param uuid A Java {@link UUID}
+     * @return The equivalent protobuf UUID (MSB, LSB)
+     */
     public static ByteString toBuf(UUID uuid) {
         if (uuid == null) {
             return ByteString.EMPTY;
